177456edadf72ffa177da10fdfa1446ea990de18
Mario Rodríguez <ruchoserrucho@hotmail.com>
2016-01-26 17:22:42 +0100

    fichero nuevo d etest

---
+++ POC_VNEXT_1/POC_VNEXT_1.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace POC_VNEXT_1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
